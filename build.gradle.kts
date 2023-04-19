22030a1b013a6010b9fd9321a7978e02c1e4d414
Android dev <androidedu@google.com>
2023-04-19 11:38:40 -0700

    Gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,13 +15,13 @@
  */
 
 buildscript {
-    extra.apply {
-        set("compose_compiler_version", "1.5.3")
+    ext {
+        compose_compiler_version = '1.4.3'
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id 'com.android.application' version '8.0.0' apply false
+    id 'com.android.library' version '8.0.0' apply false
+    id 'org.jetbrains.kotlin.android' version '1.8.10' apply false
 }
