b1fc24930d4de7576ebf05d3a162acb4fa32ba6d
Leonan Marques <leonan.marques886@gmail.com>
2024-01-01 16:56:41 -0300

    função remember foi adicionada para a variável que armazena o valor da conta

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -33,6 +33,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -94,9 +99,9 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
-    val amountInput = "0.00"
-    TextField(value = "",
-        onValueChange = {},
+    var amountInput by remember { mutableStateOf("") }
+    TextField(value = amountInput,
+        onValueChange = {amountInput = it},
         modifier = modifier)
 }
 
